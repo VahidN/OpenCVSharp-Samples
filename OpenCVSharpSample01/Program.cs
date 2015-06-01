02031b42d93bfae0d608715302b41f555dace60b
VahidN <vahid_nasiri@yahoo.com>
2015-06-01 14:51:41 +0430

    Init.

---
+++ OpenCVSharpSample01/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenCvSharp;
+
+namespace OpenCVSharpSample01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var img = Cv.CreateImage(new CvSize(128, 128), BitDepth.U8, 1);
+
+            for (var y = 0; y < img.Height; y++)
+            {
+                for (var x = 0; x < img.Width; x++)
+                {
+                    Cv.Set2D(img, y, x, x + y);
+                }
+            }
+
+            Cv.NamedWindow("window");
+            Cv.ShowImage("window", img);
+            Cv.WaitKey();
+            Cv.DestroyWindow("window");
+
+            Cv.ReleaseImage(img);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+OpenCVSharp Samples
+=======
+A collection of samples about using OpenCV in .NET applications.
