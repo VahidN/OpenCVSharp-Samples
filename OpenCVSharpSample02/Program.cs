0c032b2ac1b3db29061226a80af83616a0309794
VahidN <vahid_nasiri@yahoo.com>
2015-06-02 11:08:12 +0430

    Added IplImage.FromFile.

--- OpenCVSharpSample02/Program.cs
+++ OpenCVSharpSample02/Program.cs
@@ -8,6 +8,7 @@ namespace OpenCVSharpSample02
         {
             readImage1();
             readImage2();
+            readImage3();
         }
 
         private static void readImage1()
@@ -27,6 +28,19 @@ namespace OpenCVSharpSample02
         private static void readImage2()
         {
             using (var img = new IplImage(@"..\..\images\ocv02.jpg", LoadMode.Unchanged))
+            {
+                using (var window = new CvWindow("window"))
+                {
+                    window.Image = img;
+                    Cv.WaitKey();
+                }
+            }
+        }
+
+        private static void readImage3()
+        {
+            // it uses `System.Drawing.Bitmap` behind the scene.
+            using (var img = IplImage.FromFile(@"..\..\images\ocv02.jpg", LoadMode.Unchanged))
             {
                 using (var window = new CvWindow("window"))
                 {
