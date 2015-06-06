c94431a7655f352d50a471161c11103df32f988a
VahidN <vahid_nasiri@yahoo.com>
2015-06-06 10:33:14 +0430

    Added CvCapture Sample.

---
+++ OpenCVSharpSample06WinForms/FrmMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace OpenCVSharpSample06WinForms
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.BtnStart);
+            this.flowLayoutPanel1.Controls.Add(this.BtnStop);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(448, 328);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(3, 3);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(75, 23);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = " Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Location = new System.Drawing.Point(84, 3);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(75, 23);
+            this.BtnStop.TabIndex = 1;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 328);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "FrmMain";
+            this.Text = "OpenCV Sample";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnStop;
+    }
+}
+
---
+++ OpenCVSharpSample06WinForms/FrmMain.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel;
+using System.Threading;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.UserInterface;
+
+namespace OpenCVSharpSample06WinForms
+{
+    public partial class FrmMain : Form
+    {
+        private PictureBoxIpl _pictureBoxIpl1;
+        private BackgroundWorker _worker;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, System.EventArgs e)
+        {
+            if (_worker != null && _worker.IsBusy)
+            {
+                return;
+            }
+
+            _worker = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true
+            };
+            _worker.DoWork += workerDoWork;
+            _worker.ProgressChanged += workerProgressChanged;
+            _worker.RunWorkerCompleted += workerRunWorkerCompleted;
+            _worker.RunWorkerAsync();
+
+            BtnStart.Enabled = false;
+        }
+
+        private void BtnStop_Click(object sender, System.EventArgs e)
+        {
+            if (_worker != null)
+            {
+                _worker.CancelAsync();
+                _worker.Dispose();
+            }
+            BtnStart.Enabled = true;
+        }
+
+        private void FrmMain_Load(object sender, System.EventArgs e)
+        {
+            _pictureBoxIpl1 = new PictureBoxIpl
+            {
+                AutoSize = true
+            };
+            flowLayoutPanel1.Controls.Add(_pictureBoxIpl1);
+        }
+
+        private void workerDoWork(object sender, DoWorkEventArgs e)
+        {
+            using (var capture = new CvCapture(@"..\..\Videos\drop.avi"))
+            {
+                var interval = (int)(1000 / capture.Fps);
+
+                IplImage image;
+                while ((image = capture.QueryFrame()) != null &&
+                        _worker != null && !_worker.CancellationPending)
+                {
+                    _worker.ReportProgress(0, image);
+                    Thread.Sleep(interval);
+                }
+            }
+        }
+
+        private void workerProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            var image = e.UserState as IplImage;
+            if (image == null) return;
+
+            //Cv.Not(image, image);
+            _pictureBoxIpl1.RefreshIplImage(image);
+        }
+
+        private void workerRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            _worker.Dispose();
+            _worker = null;
+            BtnStart.Enabled = true;
+        }
+    }
+}
