55ce6e88733de2a6c3165e333d8d7bf066967cbe
VahidN <vahid_nasiri@yahoo.com>
2015-06-26 13:13:18 +0430

    Added getFps.

--- OpenCVSharpSample06WinForms/FrmMain.Designer.cs
+++ OpenCVSharpSample06WinForms/FrmMain.Designer.cs
@@ -31,6 +31,8 @@
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.BtnStart = new System.Windows.Forms.Button();
             this.BtnStop = new System.Windows.Forms.Button();
+            this.RadioAvi = new System.Windows.Forms.RadioButton();
+            this.RadioWebCam = new System.Windows.Forms.RadioButton();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -38,6 +40,8 @@
             // 
             this.flowLayoutPanel1.Controls.Add(this.BtnStart);
             this.flowLayoutPanel1.Controls.Add(this.BtnStop);
+            this.flowLayoutPanel1.Controls.Add(this.RadioAvi);
+            this.flowLayoutPanel1.Controls.Add(this.RadioWebCam);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
@@ -64,6 +68,28 @@
             this.BtnStop.UseVisualStyleBackColor = true;
             this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
             // 
+            // RadioAvi
+            // 
+            this.RadioAvi.AutoSize = true;
+            this.RadioAvi.Checked = true;
+            this.RadioAvi.Location = new System.Drawing.Point(165, 3);
+            this.RadioAvi.Name = "RadioAvi";
+            this.RadioAvi.Size = new System.Drawing.Size(58, 17);
+            this.RadioAvi.TabIndex = 2;
+            this.RadioAvi.TabStop = true;
+            this.RadioAvi.Text = "AVI file";
+            this.RadioAvi.UseVisualStyleBackColor = true;
+            // 
+            // RadioWebCam
+            // 
+            this.RadioWebCam.AutoSize = true;
+            this.RadioWebCam.Location = new System.Drawing.Point(229, 3);
+            this.RadioWebCam.Name = "RadioWebCam";
+            this.RadioWebCam.Size = new System.Drawing.Size(69, 17);
+            this.RadioWebCam.TabIndex = 3;
+            this.RadioWebCam.Text = "WebCam";
+            this.RadioWebCam.UseVisualStyleBackColor = true;
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -74,6 +100,7 @@
             this.Text = "OpenCV Sample";
             this.Load += new System.EventHandler(this.FrmMain_Load);
             this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -83,6 +110,8 @@
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button BtnStart;
         private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.RadioButton RadioAvi;
+        private System.Windows.Forms.RadioButton RadioWebCam;
     }
 }
 
--- OpenCVSharpSample06WinForms/FrmMain.cs
+++ OpenCVSharpSample06WinForms/FrmMain.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics;
 using System.Threading;
 using System.Windows.Forms;
 using OpenCvSharp;
@@ -16,6 +17,25 @@ namespace OpenCVSharpSample06WinForms
             InitializeComponent();
         }
 
+        private static double getFps(CvCapture capture)
+        {
+            double counter = 0;
+            double seconds = 0;
+            var watch = Stopwatch.StartNew();
+            while (capture.QueryFrame() != null)
+            {
+                counter++;
+                seconds = watch.ElapsedMilliseconds / (double)1000;
+                if (seconds >= 3)
+                {
+                    watch.Stop();
+                    break;
+                }
+            }
+            var fps = counter / seconds;
+            return fps;
+        }
+
         private void BtnStart_Click(object sender, System.EventArgs e)
         {
             if (_worker != null && _worker.IsBusy)
@@ -28,7 +48,17 @@ namespace OpenCVSharpSample06WinForms
                 WorkerReportsProgress = true,
                 WorkerSupportsCancellation = true
             };
-            _worker.DoWork += workerDoWork;
+
+            if (RadioAvi.Checked)
+            {
+                _worker.DoWork += workerDoReadVideo;
+            }
+
+            if (RadioWebCam.Checked)
+            {
+                _worker.DoWork += workerDoReadCamera;
+            }
+
             _worker.ProgressChanged += workerProgressChanged;
             _worker.RunWorkerCompleted += workerRunWorkerCompleted;
             _worker.RunWorkerAsync();
@@ -55,7 +85,25 @@ namespace OpenCVSharpSample06WinForms
             flowLayoutPanel1.Controls.Add(_pictureBoxIpl1);
         }
 
-        private void workerDoWork(object sender, DoWorkEventArgs e)
+        private void workerDoReadCamera(object sender, DoWorkEventArgs e)
+        {
+            using (var capture = CvCapture.FromCamera(index: 0))
+            {
+                var fps = getFps(capture);
+                capture.SetCaptureProperty(CvConst.CV_CAP_PROP_FPS, fps);
+                var interval = (int)(1000 / fps);
+
+                IplImage image;
+                while ((image = capture.QueryFrame()) != null &&
+                        _worker != null && !_worker.CancellationPending)
+                {
+                    _worker.ReportProgress(0, image);
+                    Thread.Sleep(interval);
+                }
+            }
+        }
+
+        private void workerDoReadVideo(object sender, DoWorkEventArgs e)
         {
             using (var capture = new CvCapture(@"..\..\Videos\drop.avi"))
             {
